4b09fd5e0bea63448425a894c82377412d42ab67
Alya-N <42499495+Alya-N@users.noreply.github.com>
2018-12-20 01:55:53 +0300

    4.0.0.1b_test_build
    May have fixed some things. Still not tested, but it runs.

--- cosmosData.h
+++ cosmosData.h
@@ -14,7 +14,7 @@
 #include <fstream>
 
 // Version number not used anywhere except in output to know immediately which version the user is running
-const std::string VERSION = "4.0.0.0a";
+const std::string VERSION = "4.0.0.1b_test_build";
 
 const size_t GIGABYTE = ((size_t) (1) << 30);
 
